7cd2968862d4a59c03124699263d4078be242ef6
Mohammad Albuainain <m.albuainain@tuta.io>
2026-08-27 08:32:21 +0300

    feat: register User model with Unfold admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from unfold.admin import ModelAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class useradmin(ModelAdmin):
+    list_display = ["username", "full_name" ,"role"]
+    list_filter = [ "role"]
+    search_fields = ["username", "full_name", "national_id"]
