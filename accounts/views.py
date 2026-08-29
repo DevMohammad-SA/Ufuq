4f4462e607a55f87091afbcb336ad285a41f686d
Mohammad Albuainain <dev.mohammad@tuta.io>
2026-08-29 17:49:42 +0300

    feat: redirect users to role-specific dashboard after login

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,29 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
 from .forms import ParticipantAuthenticationForm
+from .models import Role
 
 
 class ParticipantLoginView(LoginView):
     template_name = "accounts/login_participant.html"
     form_class = ParticipantAuthenticationForm
 
+    def get_success_url(self):
+        return reverse("participants:dashboard")
+
 
 class SupervisorLoginView(LoginView):
     template_name = "accounts/login_supervisor.html"
+
+    def get_success_url(self):
+        role = self.request.user.role
+        if role == Role.GROUP_SUPERVISOR:
+            return reverse("participants:supervisor_dashboard")
+        return reverse("home")
 
 
 class RahhalLogoutView(LogoutView):
