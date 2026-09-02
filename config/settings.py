8b0bf8cecd8f586a0f06afe6f0fd2140989a979f
Mohammad Albuainain <dev.mohammad@tuta.io>
2026-09-02 23:02:38 +0300

    fix: configure MEDIA_URL and MEDIA_ROOT for uploaded submission files

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,6 @@ USE_TZ = True
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="home.html"), name="home"),
-    path('accounts/', include('accounts.urls')),
-    path('participants/', include('participants.urls')),
+    path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("accounts/", include("accounts.urls")),
+    path("participants/", include("participants.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
